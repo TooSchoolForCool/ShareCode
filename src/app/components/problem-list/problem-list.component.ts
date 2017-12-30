2ce4f96a4646814bf57d6cec7cab1b9b943ebb1d
TooSchoolForCool <zeyuz@outlook.com>
2017-12-29 17:11:47 -0800

    Add routes

--- src/app/components/problem-list/problem-list.component.ts
+++ src/app/components/problem-list/problem-list.component.ts
@@ -1,24 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { Problem } from "../../models/problem.model";
-
-const PROBLEMS: Problem[] = [
-  {
-    id: 1,
-    name: "Watermelon",
-    desc: `One hot summer day Pete and his friend Billy decided to buy a watermelon. They chose the biggest and the ripest one, in their opinion. After that the watermelon was weighed, and the scales showed w kilos. They rushed home, dying of thirst, and decided to divide the berry, however they faced a hard problem.
-
-Pete and Billy are great fans of even numbers, that's why they want to divide the watermelon in such a way that each of the two parts weighs even number of kilos, at the same time it is not obligatory that the parts are equal. The boys are extremely tired and want to start their meal as soon as possible, that's why you should help them and find out, if they can divide the watermelon in the way they want. For sure, each of them should get a part of positive weight.`,
-    difficulty: "Easy"
-  },
-  {
-    id: 2,
-    name: "Two Sum",
-    desc: `Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.`,
-    difficulty: "Medium"
-  }
-];
 
 @Component({
   selector: 'app-problem-list',
@@ -29,10 +10,13 @@ You may assume that each input would have exactly one solution, and you may not 
 export class ProblemListComponent implements OnInit {
   problems: Problem[];
 
-  constructor() { }
+  constructor(@Inject("data") private data) { }
 
   ngOnInit() {
-    this.problems = PROBLEMS;
+    this.getProblems();
   }
 
+  getProblems(): void {
+    this.problems = this.data.getProblems();
+  }
 }
