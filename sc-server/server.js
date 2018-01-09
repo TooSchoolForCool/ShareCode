723461a5489ea38a5553eb6f60d594745f117919
Zeyu Zhang <zeyuz@outlook.com>
2018-01-09 12:32:41 -0800

    Bug fixed and improve performance

--- sc-server/server.js
+++ sc-server/server.js
@@ -10,22 +10,26 @@ var mongoose = require('mongoose');
 var rest_router = require("./routes/rest");
 
 // add webpage indexing router
-var index_router = require("./routes/index");
+var client_index_router = require("./routes/index");
 
 // connect to remote database
 mongoose.connect('mongodb://sc-user:123456@ds247587.mlab.com:47587/sharecode-db');
 
 // enable webpage route router
-app.use('/', rest_router);
-
+app.use('/', client_index_router);
 // all static webpage data, reading from ../client-dist/
 app.use(express.static(path.join(__dirname, '../client-dist/')));
 
 // enable api router
-app.use("/api/v1", rest_router);
+app.use("/api/v1/", rest_router);
+
+// let client handle other routing path (excepts '/' and '/api/v1/')
+app.use( function(req, res) {
+  res.sendFile("index.html", {root: path.join(__dirname, '../client-dist/')});
+});
 
 // define listening port
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
-})
+});
 
